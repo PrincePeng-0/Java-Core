3070c40fd50b7888851e2f4664b088c0b8d51293
princepeng <935941784@qq.com>
2025-12-19 10:14:22 +0800

    add some comments

--- DesignPatterns/src/main/java/com/xiaoyudi/jc/dp/singleton/LazySingleton.java
+++ DesignPatterns/src/main/java/com/xiaoyudi/jc/dp/singleton/LazySingleton.java
@@ -1,11 +1,11 @@
 package com.xiaoyudi.jc.dp.singleton;
 
 public class LazySingleton {
-
+    //使用volatile 防止cpu指令重排问题
     private static volatile LazySingleton instance;
-
+    //创建私有构造函数， 防止外部new
     private LazySingleton(){}
-
+    //获取单例对象， 使用双重检查提升性能
     public static LazySingleton getInstance(){
         if(instance == null){
             synchronized (LazySingleton.class){
